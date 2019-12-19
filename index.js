9f1445857daa95a79686726eeef8d7293fbd6723
Ariel Davis <aadavisbusiness@gmail.com>
2019-12-19 17:02:40 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
 function dwarfRollCall(dwarves) {
+  var arr = []
+  for(var i = 0; i < dwarves.length; i++) {
+    arr.push(`${i + 1}. ${dwarves[i]} `)
+  } return `${arr.join('')}`
 }
 
 function summonCaptainPlanet(planeteerCalls){
+  var arr = []
+  for(var i = 0; i<planeteerCalls.length; i++) {
+   arr.push(`${planeteerCalls[i].toUpperCase()}!`)
+ } return arr
 }
 
 function longPlaneteerCalls(words) {
+  for(var i = 0; i<words.length; i++) {
+    if(words.length > 4) {
+      return true
+    } else return false
+  }
 }
 
 function findTheCheese (foods) {
+  for(var i = 0; i<foods.length; i++) {
+    if(foods[i] == 'cheddar' || foods[i] == 'gouda' || foods[i] == 'camembert') {
+      return foods[i]
+    }
+  } return 'no cheese!'
 }
